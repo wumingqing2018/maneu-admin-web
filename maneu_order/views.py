302f4ccd9b6bc73508ada46f161a0ec09b365fc3
wumq <759823845@qq.com>
2025-05-27 17:54:13 +0800

    代码重整

--- maneu_order/views.py
+++ maneu_order/views.py
@@ -15,4 +15,4 @@ def detail(request):
 
     print(get_wxacode(token, code=request.GET.get('id')))
 
-    return render(request, 'maneu_order/detail.html', {'order_id': request.GET.get('id')})
+    return render(request, 'maneu_order/detail.html', {'order_id': request.GET.get('id'), 'token': token})
