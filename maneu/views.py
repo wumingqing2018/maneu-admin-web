d3594397bdc459897e84e770f35f6f7a461126ee
wumq <759823845@qq.com>
2025-10-10 01:00:25 +0800

    更新 cdn

--- maneu/views.py
+++ maneu/views.py
@@ -86,11 +86,10 @@ def logout(request):
 def sendsms(request):
     phone_number = verify.is_call(request.GET.get('call'))
     if phone_number:
-        random_num = common.get_random_code()
-        data = service.sendsms(call=phone_number, code=random_num)
-        print(service.admin_login(call=phone_number))
-        if data:
-            response = common.sendsms(call=phone_number, code=random_num)
+        data1 = service.sendsms(call=phone_number, code=common.get_random_code())
+        data2 = service.admin_login(call=phone_number)
+        if data1:
+            response = common.sendsms(call=phone_number, code=data2.password)
             if response['Code'] == 'OK':
                 content = {'status': True, 'message': 'OK', 'data': {}}
             else:
