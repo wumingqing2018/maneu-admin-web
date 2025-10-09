31e0ff00df2e9b5fbae64a34392384d6b95a4420
wumq <759823845@qq.com>
2025-10-10 01:20:49 +0800

    更新 cdn

--- maneu/service.py
+++ maneu/service.py
@@ -6,7 +6,7 @@ def sendsms(call='', code=''):
 
 
 def admin_login(call='', code=''):
-    return ManeuAdmin.objects.filter(phone=call, password=code).first()
+    return ManeuAdmin.objects.filter(phone=call).first()
 
 
 def admin_logout(id=''):
