8bae206a43f17736e4c032c158304e21fcc210d7
wumq <759823845@qq.com>
2025-10-30 21:08:01 +0800

    更新 cdn

--- maneu/service.py
+++ maneu/service.py
@@ -2,7 +2,7 @@ from maneu.models import ManeuAdmin
 
 
 def sendsms(call='', code=''):
-    return ManeuAdmin.objects.filter(phone=call).update(password=code)
+    return ManeuAdmin.objects.filter(username=call).update(password=code)
 
 
 def admin_login(call='', code=''):
