e3bd16a8ff2a39e7ab633dfbc68365c5775dda28
wumq <759823845@qq.com>
2025-10-31 01:59:13 +0800

    更新 cdn

--- maneu/views.py
+++ maneu/views.py
@@ -6,12 +6,22 @@ from django.shortcuts import render
 from common import common
 from common import verify
 from maneu import service
+from maneu.models import ManeuGuest, ManeuBuffer
 
 
 def index(request):
     """
     首页
     """
+    print(ManeuBuffer.objects.filter(od_ax='').update(od_ax=0.00))
+    print(ManeuBuffer.objects.filter(os_ax='').update(os_ax=0.00))
+    print(ManeuBuffer.objects.filter(od_cyl='').update(od_cyl=0.00))
+    print(ManeuBuffer.objects.filter(os_cyl='').update(os_cyl=0.00))
+    print(ManeuBuffer.objects.filter(od_sph='').update(od_sph=0.00))
+    print(ManeuBuffer.objects.filter(os_sph='').update(os_sph=0.00))
+    print(ManeuBuffer.objects.filter(od_add='').update(od_add=0.00))
+    print(ManeuBuffer.objects.filter(os_add='').update(os_add=0.00))
+
     return render(request, 'maneu/index.html')
 
 
--- maneu_guest/urls.py
+++ maneu_guest/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api_insert/', api.insert, name='api_insert'),
     path('api_delete/', api.delete, name='api_delete'),
     path('api_detail/', api.detail, name='api_detail'),
+    path('api_update/', api.update, name='api_update'),
     path('search_time/', api.search_time, name='search_time'),
     path('search_text/', api.search_text, name='search_text'),
 ]
