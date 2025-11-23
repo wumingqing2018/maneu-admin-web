bf99e1c2d9babdb2c0076196873c275be2307fbc
wumq <759823845@qq.com>
2025-11-23 17:41:40 +0800

    更新 cdn

--- maneu/views.py
+++ maneu/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from maneu.models import *
 
 
 def index(request):
+
+    orderList = ManeuOrder.objects.all()
+    for order in orderList:
+        print(ManeuReport.objects.filter(id=order.report_id).update(status=3))
+    print(ManeuReport.objects.filter(status__isnull=True).update(status=2))
+
     return render(request, 'maneu/index.html')
 
 
