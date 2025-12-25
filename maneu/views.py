ccea49764909c5b36f1272cb214cf808e82aaf8f
wumq <759823845@qq.com>
2025-12-25 19:35:38 +0800

    更新 cdn

--- maneu/views.py
+++ maneu/views.py
@@ -11,17 +11,20 @@ def index(request):
         guest = ManeuGuest.objects.filter(id=order.guest_id).first()
         guest2 = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name, phone=guest.phone, status=3, sex=guest.sex, age=guest.age, dfh=guest.dfh, ot=guest.ot, em=guest.em, remark=guest.remark)
         print(ManeuReport.objects.filter(id=order.report_id).update(status=3, guest_id=guest2.id))
+        order.guest_id = guest2.id
+        order.save()
 
     reportList = ManeuReport.objects.exclude(status=3).all()
     for report in reportList:
+        guest = ManeuGuest.objects.filter(id=report.guest_id).first()
         try:
-            guest = ManeuGuest.objects.filter(id=report.guest_id).first()
             guest2 = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name,
                                               phone=guest.phone, status=2, sex=guest.sex, age=guest.age, dfh=guest.dfh,
                                               ot=guest.ot, em=guest.em, remark=guest.remark)
-            ManeuReport.objects.filter(id=report.id).update(status=2, guest_id=guest2.id)
         except:
-            print(report.id)
+            guest2 = ManeuGuest.objects.create(admin_id=report.admin_id, time=report.time, name=report.name, phone=report.phone, status=2, remark=report.remark)
+            print(guest2)
+        ManeuReport.objects.filter(id=report.id).update(status=2, guest_id=guest2.id)
 
     ManeuGuest.objects.filter(status='').all().delete()
 
