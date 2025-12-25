451460d7778a125f0bb422ac7fa54ad16752d7ea
wumq <759823845@qq.com>
2025-12-25 19:05:52 +0800

    更新 cdn

--- maneu/views.py
+++ maneu/views.py
@@ -4,24 +4,26 @@ from maneu.models import *
 
 
 def index(request):
+    ManeuGuest.objects.filter().all().update(status='')
+
     orderList = ManeuOrder.objects.all()
     for order in orderList:
         guest = ManeuGuest.objects.filter(id=order.guest_id).first()
-        guest = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name, phone=guest.phone, status=3, sex=guest.sex, age=guest.age, dfh=guest.dfh, ot=guest.ot, em=guest.em, remark=guest.remark)
-        print(ManeuReport.objects.filter(id=order.report_id).update(status=3, guest_id=guest.id))
+        guest2 = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name, phone=guest.phone, status=3, sex=guest.sex, age=guest.age, dfh=guest.dfh, ot=guest.ot, em=guest.em, remark=guest.remark)
+        print(ManeuReport.objects.filter(id=order.report_id).update(status=3, guest_id=guest2.id))
 
     reportList = ManeuReport.objects.exclude(status=3).all()
     for report in reportList:
         try:
             guest = ManeuGuest.objects.filter(id=report.guest_id).first()
-            guest = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name,
-                                              phone=guest.phone, status=3, sex=guest.sex, age=guest.age, dfh=guest.dfh,
+            guest2 = ManeuGuest.objects.create(admin_id=guest.admin_id, time=guest.time, name=guest.name,
+                                              phone=guest.phone, status=2, sex=guest.sex, age=guest.age, dfh=guest.dfh,
                                               ot=guest.ot, em=guest.em, remark=guest.remark)
-            ManeuReport.objects.filter(id=report.id).update(status=2, guest_id=guest.id)
+            ManeuReport.objects.filter(id=report.id).update(status=2, guest_id=guest2.id)
         except:
             print(report.id)
 
-
+    ManeuGuest.objects.filter(status='').all().delete()
 
     return render(request, 'maneu/index.html')
 
