e36935c799c47ce13d176aaf5a44592c45deca38
wumq <759823845@qq.com>
2026-01-27 11:41:25 +0800

    更新 cdn

--- static/js/maneu_index/echarts.js
+++ static/js/maneu_index/echarts.js
@@ -1,10 +1,10 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports', 'static/javascript/maneu_index/echarts'], factory);
+        define(['exports', 'static/js/maneu_index/echarts'], factory);
     } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
         // CommonJS
-        factory(exports, require('static/javascript/maneu_index/echarts'));
+        factory(exports, require('static/js/maneu_index/echarts'));
     } else {
         // Browser globals
         factory({}, root.echarts);
