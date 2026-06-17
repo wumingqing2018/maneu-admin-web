ff33ff98bbeff42694577e7d59277b943ac127cf
wumq <759823845@qq.com>
2026-06-17 15:20:27 +0800

    别动更改

--- maneu/service.py
+++ maneu/service.py
@@ -13,8 +13,8 @@ def sendsms(call='', code=''):
     return ManeuAdmin.objects.filter(username=call).update(password=code)
 
 
-def admin_login(call='', code='', mark=''):
-    return ManeuAdmin.objects.filter(username=call, password=code).update(password=mark)
+def admin_login(call='', code=''):
+    return ManeuAdmin.objects.filter(username=call, password=code).first()
 
 
 def admin_logout(id=''):
